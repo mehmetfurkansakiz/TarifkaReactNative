7310ce4b2ca919476d566d5f315eaaecc7330e6f
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-11-14 03:38:28 +0300

    Category card and page

--- src/components/CategoryCard/CategoryCard.style.js
+++ src/components/CategoryCard/CategoryCard.style.js
@@ -20,6 +20,7 @@ export default StyleSheet.create({
     height: 100,
     width: 100,
     resizeMode: 'contain',
+    marginLeft: 20,
   },
   title: {
     color: 'black',
--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,9 +1,7 @@
-import React, {useEffect} from 'react';
-import {FlatList, Text} from 'react-native';
+import React from 'react';
+import {ActivityIndicator, FlatList, Text} from 'react-native';
 import CategoryCard from '../../components/CategoryCard/CategoryCard';
 import useFetch from '../../hooks/useFetch/useFetch';
-// import Error from '../../components/Error/Error';
-// import Loading from '../../components/Loading/Loading';
 import Config from 'react-native-config';
 import styles from './Category.style';
 
@@ -22,14 +20,12 @@ const Category = ({navigation}) => {
     navigation.navigate('FoodPage', {strCategory});
   };
 
-  if (error) {
-    // return <Error />;
-    <Text>Error!</Text>;
+  if (loading) {
+    return <ActivityIndicator size="large" />;
   }
 
-  if (loading) {
-    // return <Loading />;
-    <Text>Loading!</Text>;
+  if (error) {
+    return <Text>{error}</Text>;
   }
 
   return (
