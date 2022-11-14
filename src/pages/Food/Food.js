01113161dbdc86154f1828d924083f5bb4fc7050
mehmetfurkansakiz <62005335+Furkanium@users.noreply.github.com>
2022-11-14 03:39:58 +0300

    Food page update

--- src/pages/Food/Food.js
+++ src/pages/Food/Food.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {FlatList, Text} from 'react-native';
+import {ActivityIndicator, FlatList, Text} from 'react-native';
 import FoodCard from '../../components/FoodCard/FoodCard';
 import useFetch from '../../hooks/useFetch/useFetch';
-// import Error from '../../components/Error/Error';
-// import Loading from '../../components/Loading/Loading';
 import Config from 'react-native-config';
 import styles from './Food.style';
 
 const Food = ({route, navigation}) => {
-  console.log(route.params);
   const {strCategory} = route.params;
-  console.log(strCategory);
   const {data, loading, error} = useFetch(`${Config.API_FOOD}${strCategory}`);
 
   const handleFoodSelect = idMeal => {
@@ -21,14 +17,12 @@ const Food = ({route, navigation}) => {
     <FoodCard food={item} onSelect={() => handleFoodSelect(item.idMeal)} />
   );
 
-  if (error) {
-    // return <Error />;
-    <Text>Error!</Text>;
+  if (loading) {
+    return <ActivityIndicator size="large" />;
   }
 
-  if (loading) {
-    // return <Loading />;
-    <Text>Loading!</Text>;
+  if (error) {
+    return <Text>{error}</Text>;
   }
 
   return (
